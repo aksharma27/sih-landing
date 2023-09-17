4dbd43348633a8cd6730b18b73b717498350c2a6
abhishek <aksharma.27mjm@gmail.com>
2023-09-17 13:18:47 +0530

    features

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './nav.css';
 import Logo from '../assets/logo.png';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 
 
 const Nav = () => {
@@ -16,10 +15,10 @@ const Nav = () => {
                 <nav>
                     <ul className='nav-links'>
                         <li>Home</li>
-                        <li>About us</li>
                         <li>Features</li>
+                        <li>About us</li>
                         <li>Work</li>
-                        <li>Benifits</li>
+                        <li>Benefits</li>
                         <li>Testimonials</li>
                         <li>Contact Us</li>
                     </ul>
@@ -51,6 +50,22 @@ const Nav = () => {
                 <button>
                     Download App
                 </button>
+            </div>
+
+
+            {/* ---------------------Features------------------------------ */}
+            <div className="features">
+                <div className="features-content">
+                    {/* in features-content, we have the blue box for listed items with margin left of about 60% */}
+                    <h3>Key Features</h3>
+
+                    <ul>
+                        <li>Key Features: Instant Booking: Book medical appointments at your convenience, 24/7, without the need to make a phone call.
+                            <li> Real-Time Updates: Receive instant notifications and reminders, so you never miss an appointment again.</li>
+                            <li>Reduce Wait Times: Our intelligent scheduling system optimizes clinic flow, minimizing your time spent in waiting rooms.</li>
+                            <li>Secure Access: Access your medical records, test results, and prescriptions securely from your smartphone.</li></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
