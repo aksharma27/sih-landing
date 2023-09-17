67650e847b12d8986d2ffac254b6b03b8bcc2cf1
Akash-akp <akashakp0037@gmail.com>
2023-09-17 15:37:55 +0530

    Work

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from "./Nav";
 import Footer from './Footer';
+import Work from './Work';
 
 const Home = () => {
   return (
     <div>
         <Nav/>
         <Footer/>
+        <Work />
     </div>
   )
 }
---
+++ src/Components/Work.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <div className='work-wrapper'>
+            <h3>How App will Work??</h3>
+            <img src="../assets/FLowChart.png" alt="" />
+            <img src="../assets/Vector1.png" alt="" />
+            <img src="../assets/Vector1.png" alt="" />
+        </div>
+    );
+}
