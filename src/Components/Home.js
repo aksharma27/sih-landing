7cc73e06671ebf923ee1206027170343f5619b62
Akash-akp <akashakp0037@gmail.com>
2023-09-17 15:40:53 +0530

    Work

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Nav from "./Nav";
-import Footer from './Footer';
-import Work from './Work';
+// import Footer from './Component/Footer.jsx';
+// import Work from './Component/Work.jsx';
 
 const Home = () => {
   return (
     <div>
         <Nav/>
-        <Footer/>
-        <Work />
+        {/* <Footer/> */}
+        {/* <Work /> */}
     </div>
   )
 }
